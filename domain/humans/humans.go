1c4ee220074e6cd155a580b428e43e85b7851399
José Manuel Miranda Villagrán <androidgalaxys42@gmail.com>
2022-05-19 14:49:35 -0500

    feat: add repository to persist humans

--- domain/humans/humans.go
+++ domain/humans/humans.go
@@ -9,3 +9,8 @@ type Human struct {
 	Dna      string `gorm:"column:dna"`
 	IsMutant bool   `gorm:"column:is_mutant"`
 }
+
+// TableName overrides the table name used by Human to `humans`.
+func (Human) TableName() string {
+	return "humans"
+}
