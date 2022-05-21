ca1f4f821f785f279ecc8b7fc6f4fe05f8df3c71
José Manuel Miranda Villagrán <androidgalaxys42@gmail.com>
2022-05-20 19:55:06 -0500

    fix: add type of float64 to marshal with two decimals

--- utils/types/types_test.go
+++ utils/types/types_test.go
@@ -9,3 +9,25 @@ import (
 func TestTypes(t *testing.T) {
 	assert.EqualValues(t, "X-Request-Id", XRequestId)
 }
+
+func TestMarshalFloat(t *testing.T) {
+	f := Float(1.23)
+	s, err := f.MarshalJSON()
+	assert.Nil(t, err)
+	assert.EqualValues(t, "1.23", string(s))
+
+	f = Float(1.23456789)
+	s, err = f.MarshalJSON()
+	assert.Nil(t, err)
+	assert.EqualValues(t, "1.23", string(s))
+
+	f = Float(1)
+	s, err = f.MarshalJSON()
+	assert.Nil(t, err)
+	assert.EqualValues(t, "1.00", string(s))
+
+	f = Float(0)
+	s, err = f.MarshalJSON()
+	assert.Nil(t, err)
+	assert.EqualValues(t, "0.00", string(s))
+}
